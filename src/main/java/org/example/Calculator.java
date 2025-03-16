9c6f85c2e79d6441cc0e95119fb04a2f840df2ff
spratth <stavroulaprt@gmail.com>
2025-03-16 23:58:14 +0200

    JUnit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int div(int a, int b) throws Exception {
+        if (b == 0) throw new Exception("Div error");
+        return a / b;
+    }
+}
